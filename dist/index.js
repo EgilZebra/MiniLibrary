3205cf57d54c235010570fc95ee9debfc9dfa794
Egil Ramsten <egil.ramsten@hotmail.com>
2024-01-04 22:27:09 +0100

    Final update, All the files is finalized and ready for turn-in.

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,154 @@
-var TESTING = "Testar";
+// ~ Egil Ramsten Typescript 2024 ~ 
+;
+// Variables
+var NumberOfBooks = 0;
+var bookname = "";
+var BASE_URL = "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/books-api/books";
+var BookFolder = document.querySelector("main");
+var API_Return = [];
+// Fetch the data
+function fetchApi() {
+    fetch("".concat(BASE_URL))
+        .then(function (res) {
+        if (res.ok) {
+            return res.json();
+        }
+        else {
+            return console.log("Something Went Very Wrong!");
+        }
+    })
+        .then(function (data) {
+        API_Return = data;
+        console.log(API_Return);
+        CreateBooks(API_Return);
+    })
+        .catch(function (error) {
+        console.error(error.message);
+        console.log(error);
+    });
+}
+// Generate the books from the fetch.
+function CreateBooks(API_Return) {
+    // Create the Headline-element for the grid.
+    var GridHeader = document.createElement("div");
+    var GridHeadline = document.createElement("h1");
+    var GridLead = document.createElement("p");
+    GridHeader.classList.add("grid-header");
+    GridHeadline.textContent = "Check out our selection of children's books";
+    GridLead.textContent = "This week we can offer enhcanting stories for all ages";
+    GridHeader.appendChild(GridHeadline);
+    GridHeader.appendChild(GridLead);
+    BookFolder.appendChild(GridHeader);
+    // Create all the books in the fetch
+    for (var i = 0; i < API_Return.length; i++) {
+        var createAbook = API_Return[i];
+        // Create the elements
+        var wrapper = document.createElement("article");
+        var headline = document.createElement("h2");
+        var author = document.createElement("h3");
+        var audience = document.createElement("p");
+        // Add the values
+        headline.textContent = createAbook.title;
+        author.textContent = createAbook.author;
+        audience.textContent = createAbook.audience;
+        // append the DOM-elements
+        wrapper.appendChild(headline);
+        wrapper.appendChild(author);
+        wrapper.appendChild(audience);
+        BookFolder.appendChild(wrapper);
+        // Add the classes
+        wrapper.classList.add("book".concat(i));
+        wrapper.classList.add("book");
+        wrapper.style.backgroundColor = createAbook.color;
+        // Activate the buttons
+        wrapper.addEventListener("click", ChooseBook);
+    }
+}
+function ChooseBook() {
+    ClearMain();
+    // Change the style of mainpage
+    BookFolder.style.backgroundImage = "none";
+    BookFolder.style.backgroundColor = "rgb(47,49,49)";
+    BookFolder.style.display = "flex";
+    // Get the ID number of the Book.
+    var BookId = 0;
+    for (var i = 0; i < API_Return.length; i++) {
+        if (this.classList.contains("book".concat(i))) {
+            BookId = i;
+        }
+    }
+    // Create the elemets
+    var infowrapper = document.createElement("div");
+    var infoCard = document.createElement("div");
+    var wrapper = document.createElement("article");
+    var returnButton = document.createElement("button");
+    var ReserveBoook = document.createElement("button");
+    var optionwrapper = document.createElement("div");
+    var headlineOne = document.createElement("h2");
+    var headlineTwo = document.createElement("h2");
+    var authorOne = document.createElement("h3");
+    var authorTwo = document.createElement("h3");
+    var audience = document.createElement("p");
+    var publisher = document.createElement("p");
+    var plot = document.createElement("p");
+    var year = document.createElement("p");
+    var pages = document.createElement("p");
+    var lesserInfoCard = document.createElement("div");
+    // Add the Classes
+    infowrapper.classList.add("infowrapper");
+    infoCard.classList.add("RightInfo");
+    wrapper.classList.add("LeftInfo");
+    returnButton.classList.add("returnbutton");
+    ReserveBoook.classList.add("reservbutton");
+    optionwrapper.classList.add("optionswrapper");
+    headlineOne.classList.add("headlineSecond");
+    headlineTwo.classList.add("headlineSecond");
+    lesserInfoCard.classList.add("lesserInfoCard");
+    // Add the values
+    publisher.textContent = "Publisher: ".concat(API_Return["".concat(BookId)].publisher);
+    plot.textContent = "Synopsis: ".concat(API_Return["".concat(BookId)].plot);
+    year.textContent = "First published: ".concat(String(API_Return["".concat(BookId)].year));
+    pages.textContent = "Pages: ".concat(String(API_Return["".concat(BookId)].pages));
+    headlineOne.textContent = API_Return["".concat(BookId)].title;
+    headlineTwo.textContent = API_Return["".concat(BookId)].title;
+    authorOne.textContent = API_Return["".concat(BookId)].author;
+    authorTwo.textContent = "By: ".concat(API_Return["".concat(BookId)].author);
+    audience.textContent = "For ages: ".concat(API_Return["".concat(BookId)].audience);
+    returnButton.textContent = "Take Me Back!";
+    ReserveBoook.textContent = "Reserve This Book!";
+    // Append the DOM-elements
+    BookFolder.appendChild(infowrapper);
+    infowrapper.appendChild(wrapper);
+    infowrapper.appendChild(infoCard);
+    wrapper.appendChild(headlineOne);
+    wrapper.appendChild(authorTwo);
+    infoCard.appendChild(plot);
+    infoCard.appendChild(lesserInfoCard);
+    infoCard.appendChild(optionwrapper);
+    lesserInfoCard.appendChild(pages);
+    lesserInfoCard.appendChild(year);
+    lesserInfoCard.appendChild(audience);
+    lesserInfoCard.appendChild(publisher);
+    optionwrapper.appendChild(ReserveBoook);
+    optionwrapper.appendChild(returnButton);
+    // match the color from the API
+    wrapper.style.backgroundColor = API_Return["".concat(BookId)].color;
+    // Aktivate the returnbutton
+    returnButton.addEventListener("click", ReturnToMain);
+}
+// Clear the Mainpage of elements
+function ClearMain() {
+    while (BookFolder.hasChildNodes()) {
+        BookFolder.removeChild(BookFolder.lastChild);
+    }
+}
+// Return to and restore the mainpage.
+function ReturnToMain() {
+    ClearMain();
+    BookFolder.style.backgroundImage = "url(../src/Librarybackground.png)";
+    BookFolder.style.backgroundColor = "none";
+    BookFolder.style.display = "grid";
+    CreateBooks(API_Return);
+}
+// Run on Launch.
+fetchApi();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,189 @@
-let TESTING: string = "Testar";
+// ~ Egil Ramsten Typescript 2024 ~ 
+
+
+// Interface
+interface BOOK {
+    audience: string
+    author: string
+    color: string
+    id: number
+    pages: number
+    plot: string
+    publisher: string
+    title:string
+    year: number
+};
+
+// Variables
+let NumberOfBooks: number = 0; 
+let bookname: string = "";
+let BASE_URL: string = "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/books-api/books";
+let BookFolder = document.querySelector("main");
+let API_Return: Array<BOOK>= [];
+
+// Fetch the data
+function fetchApi(): void {
+    fetch(`${BASE_URL}`)
+    .then(res => {
+        if (res.ok) {
+            return res.json();
+        } else {
+            return console.log("Something Went Very Wrong!");      
+    }})
+    .then(data => {
+        API_Return = data;
+        console.log(API_Return);
+        CreateBooks(API_Return);
+    })
+    .catch((error) => {
+        console.error(error.message);
+        console.log(error);
+    })
+}
+
+// Generate the books from the fetch.
+function CreateBooks(API_Return: Array<BOOK>): void {
+
+    // Create the Headline-element for the grid.
+    let GridHeader = document.createElement("div")
+    let GridHeadline = document.createElement("h1");
+    let GridLead = document.createElement("p");
+    GridHeader.classList.add("grid-header")
+    GridHeadline.textContent = "Check out our selection of children's books"
+    GridLead.textContent = "This week we can offer enhcanting stories for all ages"
+    GridHeader.appendChild(GridHeadline);
+    GridHeader.appendChild(GridLead);
+    BookFolder.appendChild(GridHeader);
+
+    // Create all the books in the fetch
+    for (let i: number = 0; i < API_Return.length; i++) {
+        let createAbook: BOOK = API_Return[i];
+        
+        // Create the elements
+        let wrapper = document.createElement("article");
+        let headline = document.createElement("h2");
+        let author = document.createElement("h3");
+        let audience = document.createElement("p");
+
+        // Add the values
+        headline.textContent = createAbook.title;
+        author.textContent = createAbook.author;
+        audience.textContent = createAbook.audience;
+
+        // append the DOM-elements
+        wrapper.appendChild(headline);
+        wrapper.appendChild(author);
+        wrapper.appendChild(audience);
+        BookFolder.appendChild(wrapper);
+
+        // Add the classes
+        wrapper.classList.add(`book${i}`);
+        wrapper.classList.add(`book`);
+        wrapper.style.backgroundColor = createAbook.color;
+        
+        // Activate the buttons
+        wrapper.addEventListener("click", ChooseBook)
+    }
+
+}
+
+function ChooseBook(): void {
+
+    ClearMain();
+
+    // Change the style of mainpage
+    BookFolder.style.backgroundImage = "none";
+    BookFolder.style.backgroundColor = "rgb(47,49,49)";
+    BookFolder.style.display = "flex";
+
+    // Get the ID number of the Book.
+    let BookId: number = 0;
+    for ( let i: number = 0; i < API_Return.length; i++) {
+        if (this.classList.contains(`book${i}`)) {
+            BookId = i;
+        }
+    }
+
+    // Create the elemets
+    let infowrapper = document.createElement("div");   
+    let infoCard = document.createElement("div");   
+    let wrapper = document.createElement("article");    
+    let returnButton = document.createElement("button");    
+    let ReserveBoook = document.createElement("button");   
+    let optionwrapper = document.createElement("div");    
+    let headlineOne = document.createElement("h2");    
+    let headlineTwo = document.createElement("h2");    
+    let authorOne = document.createElement("h3");
+    let authorTwo = document.createElement("h3");
+    let audience = document.createElement("p");
+    let publisher = document.createElement("p")
+    let plot = document.createElement("p")
+    let year = document.createElement("p")
+    let pages = document.createElement("p")
+    let lesserInfoCard = document.createElement("div");
+    
+    // Add the Classes
+    infowrapper.classList.add("infowrapper");
+    infoCard.classList.add("RightInfo");
+    wrapper.classList.add("LeftInfo");
+    returnButton.classList.add("returnbutton");
+    ReserveBoook.classList.add("reservbutton");
+    optionwrapper.classList.add("optionswrapper");
+    headlineOne.classList.add("headlineSecond");
+    headlineTwo.classList.add("headlineSecond");
+    lesserInfoCard.classList.add("lesserInfoCard");
+
+    // Add the values
+    publisher.textContent = `Publisher: ${API_Return[`${BookId}`].publisher}`;
+    plot.textContent = `Synopsis: ${API_Return[`${BookId}`].plot}`;
+    year.textContent = `First published: ${String(API_Return[`${BookId}`].year)}`;
+    pages.textContent = `Pages: ${String(API_Return[`${BookId}`].pages)}`;
+    headlineOne.textContent = API_Return[`${BookId}`].title;
+    headlineTwo.textContent = API_Return[`${BookId}`].title;
+    authorOne.textContent = API_Return[`${BookId}`].author;
+    authorTwo.textContent = `By: ${API_Return[`${BookId}`].author}`;
+    audience.textContent = `For ages: ${API_Return[`${BookId}`].audience}`;
+    returnButton.textContent = "Take Me Back!"
+    ReserveBoook.textContent = "Reserve This Book!"
+
+    // Append the DOM-elements
+    BookFolder.appendChild(infowrapper);
+    infowrapper.appendChild(wrapper);
+    infowrapper.appendChild(infoCard);
+    wrapper.appendChild(headlineOne);
+    wrapper.appendChild(authorTwo);
+    infoCard.appendChild(plot);
+    infoCard.appendChild(lesserInfoCard);
+    infoCard.appendChild(optionwrapper);
+    lesserInfoCard.appendChild(pages);
+    lesserInfoCard.appendChild(year);
+    lesserInfoCard.appendChild(audience);
+    lesserInfoCard.appendChild(publisher);
+    optionwrapper.appendChild(ReserveBoook);
+    optionwrapper.appendChild(returnButton);
+
+    // match the color from the API
+    wrapper.style.backgroundColor = API_Return[`${BookId}`].color;
+
+    // Aktivate the returnbutton
+    returnButton.addEventListener("click", ReturnToMain);
+}
+
+// Clear the Mainpage of elements
+function ClearMain(): void {
+    while (BookFolder.hasChildNodes()) {
+        BookFolder.removeChild(BookFolder.lastChild);
+    }
+}
+
+// Return to and restore the mainpage.
+function ReturnToMain(): void {
+    ClearMain()
+    BookFolder.style.backgroundImage = "url(../src/Librarybackground.png)";
+    BookFolder.style.backgroundColor = "none";
+    BookFolder.style.display = "grid";
+    CreateBooks(API_Return);
+}
+
+// Run on Launch.
+fetchApi()
